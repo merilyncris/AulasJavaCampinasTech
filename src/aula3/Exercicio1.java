92107a084e9442fcc60a37aebee0120c9e1a5a74
Merilyn Vulczak <merilyncris@gmail.com>
2021-01-17 11:15:15 -0300

    exercicio 1

--- src/aula3/Exercicio1.java
+++ src/aula3/Exercicio1.java
@@ -1,5 +1,22 @@
 package aula3;
 
-public class Exercicio1 {
+import java.util.Scanner;
 
+public class Exercicio1 {
+	
+	  public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+	    System.out.println("Digite um numero:");
+	    int numero = scan.nextInt();
+
+	    boolean par = numero % 2 == 0;
+
+	    if(par) {
+	      System.out.println("Seu numero é par");
+	    } else if (!par) {
+	       System.out.println("Seu numero é impar");
+	    }
+
+	    scan.close();
+	}
 }
