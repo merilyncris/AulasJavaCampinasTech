dffd8c6a7086711b9862be90815c0d68bfac0611
Merilyn Vulczak <merilyncris@gmail.com>
2021-01-17 21:48:23 -0300

    aula 2: exercicio 1

---
+++ src/aula2/Main.java
@@ -0,0 +1,17 @@
+package aula2;
+
+	// Exercício 1
+	class Main {  
+	  public static void main(String args[]) { 
+	    int y = 99;
+	    int z = 11;
+	    System.out.println("O valor inicial da minha variável Y é: " + y);
+	    System.out.println("O valor inicial da minha variável Z é: " + z);
+	    int aux = 0;
+	    aux = y;
+	    y = z;
+	    z = aux;
+	    System.out.println("O valor depois da troca da minha variável Y é: " + y);
+	    System.out.println("O valor depois da troca da minha variável Z é: " + z);
+	  } 	
+}
